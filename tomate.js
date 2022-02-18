c0aec5a9ab69e8e17d6330c897fbd7243e2731d8
Eric Mena <menaericdaniel@gmail.com>
2022-02-18 10:30:31 -0300

    Argumento que el tomate es una VERDURA

--- tomate.js
+++ tomate.js
@@ -1,1 +1,3 @@
-console.log('tomate');
+console.log('el tomate es una VERDURA, según la RAE!!!');
+
+console.log('REFERENCIA: El tomate es una de las hortalizas más consumidas a nivel mundial, por lo que existen muchos tipos de variedades, cada una de las cuales satisface una demanda en particular')
